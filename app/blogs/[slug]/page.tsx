c44ff3aae222a9fdd5d301471c223e8b822896d0
mohamed-raihan <141607655+mohamed-raihan@users.noreply.github.com>
2025-07-14 15:02:31 +0530

    type-promise

--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -2,28 +2,22 @@ import { getBlogs } from '../../sections/Blogs/blogData';
 import BlogSection from '../../sections/Blogs/BlogSection';
 import { notFound } from 'next/navigation';
 
-// type PageProps = {
-//   params: { slug: string };
-//   searchParams: { [key: string]: string | string[] | undefined };
-// }
-
 interface Props {
-  params: { slug: string };
+  params: Promise<{ slug: string }>;
 }
 
 export default async function BlogPage({ params }: Props) {
-  const { slug } = params;
-  console.log('Checking slug:', slug);
+  // Await the params Promise
+  const { slug } = await params;
   
+  console.log('Checking slug:', slug);
   const blogs = await getBlogs();
   console.log(blogs);
   
   // Find the blog by slug (case-insensitive)
   const data = blogs.find(blog => blog.slug.toLowerCase() === slug.toLowerCase());
+  
   if (!data) return notFound();
+  
   return <BlogSection {...data} />;
 }
-
-// export default function BlogPage() {
-//     return <div>BlogPage</div>;
-// }
